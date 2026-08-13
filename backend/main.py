e86a244444b3734f3435473b57da3838da2153ca
Grha Gandana P <grhagandanap@gmail.com>
2026-08-13 21:43:14 +0700

    Add recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,22 @@
 # function to print trip summary
-def print_trip_summary(destination, country, days, budget, currency, travel_month):
+from services.trip_service import get_trip_category, get_travel_season, calculate_daily_budget, recommendation_place 
+
+def print_trip_summary(destination, days, budget, currency, travel_month):
     
     print(30*"=")
     print("\nKelanaAI\n")
     print(30*"=")
     
     print(f"Destination : {destination}")
-    print(f"Country     : {country}")
     print(f"Days        : {days}")
-    print(f"Budget      : {budget}")
-    print(f"Currency    : {currency}")
-    print(f"Travel month: {travel_month}")
+    print(f"Budget      : {budget:.0f} {currency}")
+    print(f"Category    : {get_trip_category(budget)}")
+    print(f"Daily Budget: {calculate_daily_budget(budget, days):.0f} {currency}")
+    print(f"Travel Month: {travel_month}")
+    print(f"Season      : {get_travel_season(travel_month)}")
+    print(f"\nRecommended Places:")
+    for place in recommendation_place(destination):
+        print(f" - {place}")
 
 
 destination     = input("Enter destination: ")
@@ -21,4 +27,4 @@ currency        = input("Enter currency: ")
 travel_month    = input("Enter travel month: ")
 
 # invoke function
-print_trip_summary(destination, country, days, budget, currency, travel_month)
+print_trip_summary(destination, days, budget, currency, travel_month)
---
+++ backend/services/trip_service.py
@@ -0,0 +1,23 @@
+def get_trip_category(budget):
+    if budget < 1000:
+        return "Backpacker"
+    elif 1000<= budget <= 3000:
+        return "Standard"
+    else:
+        return "Luxury"
+
+def get_travel_season(month):
+    if month == "December":
+        return "Peak Season"
+    elif month == "June":
+        return "Holiday Season"
+    else:
+        return "Regular Season"
+
+def calculate_daily_budget(budget, days):
+    return budget/days
+
+def recommendation_place(destination):
+    if destination == "Japan":
+        return ['Tokyo Tower', 'Shibuya', 'Mount Fuji']
+
